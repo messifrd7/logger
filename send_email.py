8b6094de6f3e68a18597041ae9b11f38234df544
Shreyas Moudgalya <smoudgal@hawk.iit.edu>
2016-11-10 18:31:27 -0600

    Updates

--- send_email.py
+++ send_email.py
@@ -4,28 +4,33 @@ Sends an email with the provided msg
 @author: Shreyas Moudgalya{shreyas@hawk.iit.edu}
 """
 
-from datetime import datetime
+#from datetime import datetime
 import smtplib
-from email.mime.text import MIMEText
+#from email.mime.text import MIMEText
 
 def send_email(msg):
 	
-	sender = 'smoudgal@hawk.iit.edu'
-	receivers = ['shreyas@hawk.iit.edu']
+	sender = 'shreyasmoudgalya064@gmail.com'
+	receivers = ['shreyasmoudgalya009@gmail.com']
 	host = 'smtp.gmail.com'
 	port = 587
-	user = None
-	password = None
+	user = 'shreyasmoudgalya064'
+	password = 'ShrMoud123'
 
-	smtpObject = smtplib.SMTP(host,port)
+	#server = smtplib.SMTP(host,port)
+	server = smtplib.SMTP('smtp.gmail.com:587')
+	server.ehlo()
 	server.starttls() 
 	#Check if user and password defined 
 	if user and password: 
 		server.login(user, password) 
 	
 	try:
-		#smtpObject = smtplib.SMTP('localhost') #If there is a localhost SMTP Server
-		smtpObject.sendmail(sender, receivers, message)
+		#server = smtplib.SMTP('localhost') #If there is a localhost SMTP Server
+		server.sendmail(sender, receivers, message)
 		print ("Successfully sent email")
 	except SMTPException:
 		print ("Error: unable to send email")
+
+        server.close()
+
