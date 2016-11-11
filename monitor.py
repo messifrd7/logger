0d09d14232228a498a8d7cbd217d606aecabcbce
Shreyas Moudgalya <smoudgal@hawk.iit.edu>
2016-11-10 19:13:16 -0600

    minor

--- monitor.py
+++ monitor.py
@@ -10,8 +10,8 @@ from send_email import *
 import time
 GLOBAL_TOTAL = 0
 
-""" Consume the input log file
-	Invoke parseLine() for each new line of log	"""
+"""Consume the input log file
+	Invoke parseLine() for each new line of log"""
 
 def main():
 	#p=ArgumentParser()
@@ -20,7 +20,7 @@ def main():
 
 	try:
 		logfile = open("logfile.txt", "r")
-		#print "Name of the file: ", logfile.name
+		#print ("Name of the file: ", logfile.name)
 	except Exception as e:
 		print(str(e))
 
@@ -29,11 +29,12 @@ def main():
 	
 
 def parseFile(logfile):
-    logfile.seek(0,2) #Go to the end of the file
+    #logfile.seek(0,2) #Go to the end of the file
     while True:
         line = logfile.readline()
+        #print(line)
         if not line:
-            time.sleep(0.1) # Sleep briefly
+            time.sleep(0.1)
             continue
         yield line
 
@@ -44,22 +45,23 @@ def parseLine(logfile):
 	parseFileGenerator = parseFile(logfile)
 	
 	for line in parseFileGenerator:
-		items = line.split(" ")
+		item = line.replace('[',"")
+		items = item.split("]")
 		incidentTime = items[0]
 		logName = items[1]
 		levelName = items[2]
 		errorMessage = items[3]
 		
 		if levelName == "ERROR":
+			print(line)
 			global GLOBAL_TOTAL
 			GLOBAL_TOTAL += 1
 			message = """
 	##SERVER ALERT
-	##Received: {date:%Y-%m-%d %H:%M:%S.%f}
+	##Received: {incidentdate}
 	##Total Alerts: {total:7d}
 	##Log Name: {log}
-	##total_alerts: {total:7d}
-	##The Log Error is {msg}""". format(date=incidentTime,total=GLOBAL_TOTAL, log = logName, msg=errorMessage)
+	##The Log Error is {m}""".format(incidentdate=incidentTime,total=GLOBAL_TOTAL,log=logName,m=errorMessage)
 			send_email(message)
 
 			
--- send_email.py
+++ send_email.py
@@ -11,26 +11,26 @@ import smtplib
 def send_email(msg):
 	
 	sender = 'shreyasmoudgalya064@gmail.com'
-	receivers = ['shreyasmoudgalya009@gmail.com']
+	receivers = 'shreyas@hawk.iit.edu'
 	host = 'smtp.gmail.com'
 	port = 587
 	user = 'shreyasmoudgalya064'
 	password = 'ShrMoud123'
 
-	#server = smtplib.SMTP(host,port)
-	server = smtplib.SMTP('smtp.gmail.com:587')
+	server = smtplib.SMTP(host,port)
+	#server = smtplib.SMTP('smtp.gmail.com:587')
 	server.ehlo()
 	server.starttls() 
 	#Check if user and password defined 
-	if user and password: 
-		server.login(user, password) 
+	#if user and password: 
+	server.login(user, password) 
 	
 	try:
 		#server = smtplib.SMTP('localhost') #If there is a localhost SMTP Server
 		server.sendmail(sender, receivers, message)
 		print ("Successfully sent email")
-	except SMTPException:
+	#except SMTPException as smtpE:
+	except Exception as smtpE:
 		print ("Error: unable to send email")
-
-        server.close()
+	server.close()
 
